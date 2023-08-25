03d1749d0fe9d2d53b04c48a84a25a4d8952e704
kjkast <116606303+kjkast@users.noreply.github.com>
2023-08-25 09:52:55 -0400

    Add files via upload

---
+++ mega_pygame/game_button_file.py
@@ -0,0 +1,22 @@
+import menu
+import pygame as pg
+
+class game_button:
+    
+    
+
+    def __init__(self, file_name):
+        self.image = pg.image.load(f)
+        self.screen = menu.screen
+        return
+
+    def get_image(self):
+        return self.image
+
+    def switch_scene(self):
+        self.screen.fill((255, 255, 255))
+        return
+
+    def update_location(self):
+        return
+        
--- mega_pygame/menu.py
+++ mega_pygame/menu.py
@@ -1,6 +1,6 @@
 import sys
 import pygame as pg
-import tictactoe_game
+import game_button_file
 
 pg.init()
 size = width, height = 600, 600
@@ -8,8 +8,8 @@ screen = pg.display.set_mode(size)
 screen = screen.fill((255, 255, 255))
 pg.display.set_caption("Menu")
 
-tictactoe_selection = pg.image.load("tictactoeboard.png").convert()
-t = screen.blit(tictactoe_selection, (25, 50))
+tictactoe = game_button_file.game_button("tictactoeboard.png")
+t = screen.blit(tictactoe.get_image(), (25, 50))
 
 font = pg.font.Font('freesansbold.ttf', 20)
 text = font.render('KJs Collection', True, (255, 0, 0), (0, 75, 0))
@@ -22,4 +22,5 @@ while True:
         if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
             pos = pg.mouse.get_pos
             if t.collidepoint(pos):
-                tictactoe_game.switch_scene()
+                tictactoe.switch_scene()
+                break
